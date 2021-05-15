4c0604f2d35541619843c394613be01585d46d7d
Mael Saillot <mael.saillot@telecomnancy.eu>
2021-05-15 17:16:42 +0200

    [script_import.py] import table voeux

--- script_import.py
+++ script_import.py
@@ -1,3 +1,19 @@
-import panda as pd
+import openpyxl as xl
 import sqlite3 as sql
+from pathlib import Path
 
+db = sql.connect("concours.db")
+
+for classe in ("MP", "TSI", "PT", "ATS", "PC", "PSI"):
+    file = xl.load_workbook(Path(f"../PPII_ressources/data/public/listeVoeux_{classe}.xlsx"), read_only=True)
+    tab = file[file.sheetnames[0]]
+
+    with db:
+        c = db.cursor()
+        rows = tab.rows
+        next(rows)
+        for line in rows:
+            c.execute("INSERT OR IGNORE INTO voeux VALUES (?,?,?)", (line[0].value, line[2].value, line[3].value))
+    file.close()
+
+db.close()
