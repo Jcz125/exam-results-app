152bbb04cda6bd98b535cad30230992c3445d0c8
Mael Saillot <mael.saillot@telecomnancy.eu>
2021-05-18 11:49:12 +0200

    [createdb.sql] Ajout FK sur candidat(classe), remplissage civilite

--- createdb.sql
+++ createdb.sql
@@ -120,7 +120,7 @@ CREATE TABLE IF NOT EXISTS candidat(
     nom TEXT NOT NULL,
     prenom TEXT NOT NULL,
     date_de_naissance DATE,
-    classe TEXT,
+    classe INTEGER,
     puissance TEXT CHECK(puissance IN ('3/2', '5/2', '7/2')),
     voie_concours INTEGER,
     etablissement TEXT,
@@ -171,6 +171,9 @@ CREATE TABLE IF NOT EXISTS candidat(
     FOREIGN KEY(option1) REFERENCES ep_option(id),
     FOREIGN KEY(option2) REFERENCES ep_option(id),
     FOREIGN KEY(option3) REFERENCES ep_option(id),
-    FOREIGN KEY(option4) REFERENCES ep_option(id)
-    FOREIGN KEY(civ) REFERENCES civilite(code)
+    FOREIGN KEY(option4) REFERENCES ep_option(id),
+    FOREIGN KEY(civ) REFERENCES civilite(code),
+    FOREIGN KEY(classe) REFERENCES classe(code)
 );
+
+INSERT INTO civilite VALUES (1,'M.'),(2,'Mme');
